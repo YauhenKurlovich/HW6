decfc89d709aad40bf59220bae7c108b5cccdaf4
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-16 14:34:25 +0300

    added form for create and update (without logic) & modified table(delete button)

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,24 @@
- function Post() {
+ function Post(userId, id, title, body) {
     this.data;
+    this.userId = userId;
+    this.id = id;
+    this.title = title;
+    this.body  = body;
+}
+
+post = new Post();
+
+Post.prototype.Add = function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://127.0.0.1:5500/index.html', false);
+    post2 = new Post(23,101,'asdasdas','asdasdasd');
+    alert(post2);
+    alert(JSON.stringify(post2));
+    xhr.send(JSON.stringify(post2));
+    if (xhr.status != 200) {   
+        alert( xhr.status + ': ' + xhr.statusText ); 
+      } else {
+      }
 }
 
 Post.prototype.getAll = function () {
@@ -10,27 +29,10 @@ Post.prototype.getAll = function () {
       alert( xhr.status + ': ' + xhr.statusText ); 
     } else {
        this.data = JSON.parse(xhr.responseText);
-       var row;
-       var rows = document.getElementById("tableBody");
-  
-    //   for(var i = 0; i < 100; i ++)
-    //       {
-    //           row += "<tr>"
-    //           row += "<td>" + this.data[i].userId + "</td>"; 
-    //           row += "<td>" + this.data[i].id + "</td>"; 
-    //           row += "<td>" + this.data[i].title + "</td>"; 
-    //           row += "<td>" + this.data[i].body + "</td>"; 
-    //           row += "</tr>"
-    //   }
-    // rows.innerHTML = row;
     }
 }
-post = new Post();
 
-post.getAll();
  function displayAll() {
-
-
     var row = "";
      var rows = document.getElementById("tableBody");
 
@@ -41,8 +43,10 @@ post.getAll();
             row += "<td>" + post.data[i].id + "</td>"; 
             row += "<td>" + post.data[i].title + "</td>"; 
             row += "<td>" + post.data[i].body + "</td>"; 
+            row += "<td> <button> Delete </button>  </td>";
             row += "</tr>"
     }
   rows.innerHTML = row;
 }
 
+post.getAll();
