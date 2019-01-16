582bf40873c3af904e552452e0aa8d125d2e7e10
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-16 15:56:26 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -10,33 +10,39 @@ post = new Post();
 
 Post.prototype.Add = function() {
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://127.0.0.1:5500/index.html', false);
-    post2 = new Post(23,101,'asdasdas','asdasdasd');
-    alert(post2);
-    alert(JSON.stringify(post2));
-    xhr.send(JSON.stringify(post2));
+    xhr.open('POST', 'db/db.json', false);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    jsonBody = new Post(1,11,'Yauhen','Kurlovich');
+    xhr.send(JSON.stringify(jsonBody));
     if (xhr.status != 200) {   
         alert( xhr.status + ': ' + xhr.statusText ); 
       } else {
+          alert("Created");
       }
 }
 
 Post.prototype.getAll = function () {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', false);
+    xhr.open('GET', 'db/db.json', false);
     xhr.send();
     if (xhr.status != 200) {   
       alert( xhr.status + ': ' + xhr.statusText ); 
     } else {
        this.data = JSON.parse(xhr.responseText);
+       alert("Get is done");
     }
+}
+
+function Add()
+{
+    post.Add();
 }
 
  function displayAll() {
     var row = "";
      var rows = document.getElementById("tableBody");
 
-    for(var i = 0; i < 100; i ++)
+    for(var i = 0; i < post.data.length; i ++)
         {
             row += "<tr>"
             row += "<td>" + post.data[i].userId + "</td>"; 
