5793f1efa94d44db4d129c95a6307a389fe30ce0
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-17 14:25:59 +0300

    task 6 is done

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,105 @@
  function Post(userId, id, title, body) {
-    this.data;
-    this.userId = userId;
-    this.id = id;
-    this.title = title;
-    this.body  = body;
-}
+     this.data;
+     this.userId = userId;
+     this.id = id;
+     this.title = title;
+     this.body = body;
+ }
 
-post = new Post();
+ post = new Post();
 
-Post.prototype.Add = function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'db/db.json', false);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    jsonBody = new Post(1,11,'Yauhen','Kurlovich');
-    xhr.send(JSON.stringify(jsonBody));
-    if (xhr.status != 200) {   
-        alert( xhr.status + ': ' + xhr.statusText ); 
-      } else {
-          alert("Created");
-      }
-}
+ Post.prototype.Add = function () {
+     var xhr = new XMLHttpRequest();
+     xhr.open('POST', 'https://my-json-server.typicode.com/YauhenKurlovich/demo/posts', false);
+     var postData = {
+         userId: document.dataForm.uId.value,
+         title: document.dataForm.title.value,
+         body: document.dataForm.body.value
+     }
+     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+     alert(JSON.stringify(postData));
+     xhr.send(JSON.stringify(postData));
+     if (xhr.status != 200) {
+         alert(xhr.status + ': ' + xhr.statusText);
+     } else {
+         alert(xhr.status + ': ' + xhr.statusText);
+         alert("Created");
+     }
+ }
 
-Post.prototype.getAll = function () {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'db/db.json', false);
-    xhr.send();
-    if (xhr.status != 200) {   
-      alert( xhr.status + ': ' + xhr.statusText ); 
-    } else {
-       this.data = JSON.parse(xhr.responseText);
-       alert("Get is done");
-    }
-}
+ Post.prototype.Update = function () {
+     var xhr = new XMLHttpRequest();
+     xhr.open('PUT', 'https://my-json-server.typicode.com/YauhenKurlovich/demo/posts/' + document.dataForm.Id.value, false);
+     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+     var putData = {
+         Id: document.dataForm.Id.value,
+         userId: document.dataForm.uId.value,
+         title: document.dataForm.title.value,
+         body: document.dataForm.body.value
+     }
 
-function Add()
-{
-    post.Add();
-}
+     xhr.send(JSON.stringify(putData));
+     if (xhr.status != 200) {
+         alert(xhr.status + ': ' + xhr.statusText);
+     } else {
+         alert("Updated");
+         alert(xhr.status + ': ' + xhr.statusText);
+     }
+ }
+
+ Post.prototype.Delete = function () {
+     var xhr = new XMLHttpRequest();
+     xhr.open('DELETE', 'https://my-json-server.typicode.com/YauhenKurlovich/demo/posts/' + document.dataForm.Id.value, false);
+     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+     xhr.send();
+     if (xhr.status != 200) {
+         alert(xhr.status + ': ' + xhr.statusText);
+     } else {
+         alert("Deleted");
+         alert(xhr.status + ': ' + xhr.statusText);
+     }
+ }
+
+
+ Post.prototype.getAll = function () {
+     var xhr = new XMLHttpRequest();
+     xhr.open('GET', 'https://my-json-server.typicode.com/YauhenKurlovich/demo/posts', false);
+     xhr.send();
+     if (xhr.status != 200) {
+         alert(xhr.status + ': ' + xhr.statusText);
+     } else {
+         this.data = JSON.parse(xhr.responseText);
+         alert(xhr.status + ': ' + xhr.statusText);
+         alert("Get is done");
+     }
+ }
+
+ function Add() {
+     post.Add();
+ }
+
+ function Update() {
+     post.Update();
+ }
+
+ function Delete() {
+     post.Delete();
+ }
 
  function displayAll() {
-    var row = "";
+     var row = "";
      var rows = document.getElementById("tableBody");
 
-    for(var i = 0; i < post.data.length; i ++)
-        {
-            row += "<tr>"
-            row += "<td>" + post.data[i].userId + "</td>"; 
-            row += "<td>" + post.data[i].id + "</td>"; 
-            row += "<td>" + post.data[i].title + "</td>"; 
-            row += "<td>" + post.data[i].body + "</td>"; 
-            row += "<td> <button> Delete </button>  </td>";
-            row += "</tr>"
-    }
-  rows.innerHTML = row;
-}
+     for (var i = 0; i < post.data.length; i++) {
+         row += "<tr>"
+         row += "<td>" + post.data[i].userId + "</td>";
+         row += "<td>" + post.data[i].id + "</td>";
+         row += "<td>" + post.data[i].title + "</td>";
+         row += "<td>" + post.data[i].body + "</td>";
+         row += "<td> <button onclick='Delete()'> Delete </button>  </td>";
+         row += "</tr>"
+     }
+     rows.innerHTML = row;
+ }
 
-post.getAll();
+ post.getAll();
